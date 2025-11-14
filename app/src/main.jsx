e640f129f6e7e97205a32c2f60b35a96f0bedb2d
gires667 <gires8585@gmail.com>
2025-11-14 17:09:10 +0100

    Feat: add-tanstack router

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,12 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)  //
+import { routeTree } from './routeTree.gen'
 
-//
+
+
+// Crée une instance du router
+const router = createRouter({ routeTree })
+
+// Render de l'application
+const rootElement = document.getElementById('root')
+if (rootElement && !rootElement.innerHTML) {
+  const root = createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router}>
+        <TanStackRouterDevtools initialIsOpen={false} position="bottom-right" />
+      </RouterProvider>
+    </StrictMode>
+  )
+}
